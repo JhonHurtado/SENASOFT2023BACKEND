b234763ed0fe0fff34da25c0b4bd6086dd8f5826
jhon hurtado <jhonse2001@gmail.com>
2023-09-27 09:48:58 -0500

    add welcome API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import app from './src/app/app.js';
 import {PORT} from './src/config/config.js';
 
 app.use((req, res, next) => {
-    return res.status(200).json("Bienvenido a StudyBuddy");
+    return res.status(200).json("Welcome to StudyBuddy");
   });
   
 
