8e683ce2c164a93432e446586fb14058172fb333
jhon hurtado <jhonse2001@gmail.com>
2023-09-28 09:06:17 -0500

    add modify datbase

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     name:{type:String, required:true},
     lastName:{type:String, required:true},
     dateBirth:{type:Date, required:true},
-    email:{type:String, required:true},
+    email:{type:String, required:true,unique:true},
     password:{type:String, required:true},
     course:{type:Number, required:true},
     program:{type:String, required:true},
